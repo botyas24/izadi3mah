343a6c11b63e6d706f9dfcee0824f22473a91a64
botyas24 <163675231+botyas24@users.noreply.github.com>
2024-10-29 19:15:05 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '1841f3aa-6943-463a-bcfb-f79d79af19b6';
+let userID = 'f97dc122-2e89-49da-9327-5c7bd4a0b218';
 
 const พร็อกซีไอพีs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.cloudflare.cyou'];
 
